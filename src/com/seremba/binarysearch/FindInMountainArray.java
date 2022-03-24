c276bf8372d39d76c18a75cdbedc12372564069e
seremba <pserembae@gmail.com>
2022-03-24 18:07:30 +0300

    search in the rotated sorted array

--- src/com/seremba/binarysearch/FindInMountainArray.java
+++ src/com/seremba/binarysearch/FindInMountainArray.java
@@ -3,6 +3,26 @@ package com.seremba.binarysearch;
 public class FindInMountainArray {
 
 	public static void main(String[] args) {
+		int[] arr = {1, 2, 3, 1};
+		int ans = findInMountArray(arr);
+		System.out.println(ans);
+	}
 
+	static int findInMountArray(int[] arr){
+		int start = 0;
+		int end = arr.length -1;
+
+		while(start < end){
+			int mid = start + (end -start) / 2;
+			if(arr[mid] > arr[mid + 1]){
+				// you are in the decreasing part of the array
+				// this maybe the answer but look at left
+				end = mid;
+			} else {
+				// you are in the asc part of the array
+				start = mid + 1;
+			}
+		}
+		return start;
 	}
 }
